df7ddb77ced191723454770bdb80e0029a1a89aa
Remo Faller <sztuczka0magiczka@gmail.com>
2026-09-24 21:30:15 +0200

    Create a new Android app module based on the existing project structure and repurpose the app shell as a fresh NovaFlow app.

---
+++ FallerOS-Android/newapp/src/main/java/com/falleros/newapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.falleros.newapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.webkit.WebViewAssetLoader
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
+            .build()
+
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.databaseEnabled = true
+        settings.allowFileAccess = false
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+        settings.setSupportZoom(false)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldInterceptRequest(
+                view: WebView,
+                request: WebResourceRequest
+            ): WebResourceResponse? = assetLoader.shouldInterceptRequest(request.url)
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {}
+        webView.loadUrl("https://appassets.androidplatform.net/assets/www/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
--- FallerOS-Android/settings.gradle.kts
+++ FallerOS-Android/settings.gradle.kts
@@ -14,4 +14,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "FallerOS-Android"
-include(":app")
+include(":app", ":newapp")
